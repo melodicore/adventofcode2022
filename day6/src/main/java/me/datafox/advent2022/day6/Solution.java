6e3112e8ff342421719f223e2eb4c0ca023a8a3a
melodicore <datafox@datafox.me>
2023-12-08 23:25:26 +0200

    day 6 part 1

--- day6/src/main/java/me/datafox/advent2022/day6/Solution.java
+++ day6/src/main/java/me/datafox/advent2022/day6/Solution.java
@@ -2,6 +2,9 @@ package me.datafox.advent2022.day6;
 
 import me.datafox.advent2022.SolutionBase;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Solution for advent of code 2022 day 6.
  *
@@ -16,7 +19,19 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return "";
+        int result = -1;
+        for(int i = 0; i < input.length() - 3; i++) {
+            Set<Character> set = new HashSet<>();
+            for(int j = i; j < i + 4; j++) {
+                set.add(input.charAt(j));
+            }
+            System.out.println(set);
+            if(set.size() == 4) {
+                result = i + 4;
+                break;
+            }
+        }
+        return String.valueOf(result);
     }
 
     @Override
