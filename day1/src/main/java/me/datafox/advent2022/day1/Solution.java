285aa88766c5ef5794458ac72109586e39f8ecce
melodicore <datafox@datafox.me>
2023-12-08 21:03:09 +0200

    day 1 part 1

--- day1/src/main/java/me/datafox/advent2022/day1/Solution.java
+++ day1/src/main/java/me/datafox/advent2022/day1/Solution.java
@@ -2,6 +2,10 @@ package me.datafox.advent2022.day;
 
 import me.datafox.advent2022.SolutionBase;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+
 /**
  * Solution for advent of code 2022 day 1.
  *
@@ -16,7 +20,13 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return "";
+        return String.valueOf(Arrays.stream(
+                        input.split("\n\n"))
+                .mapToInt(s -> s
+                        .lines()
+                        .mapToInt(Integer::parseInt)
+                        .sum())
+                .max().orElse(-1));
     }
 
     @Override
