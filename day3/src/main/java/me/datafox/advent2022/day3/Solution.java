3dbeca00cffa14e51a4f389f769a7f40713473c4
melodicore <datafox@datafox.me>
2023-12-08 21:38:31 +0200

    day 3 part 1

--- day3/src/main/java/me/datafox/advent2022/day3/Solution.java
+++ day3/src/main/java/me/datafox/advent2022/day3/Solution.java
@@ -2,6 +2,10 @@ package me.datafox.advent2022.day3;
 
 import me.datafox.advent2022.SolutionBase;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  * Solution for advent of code 2022 day 3.
  *
@@ -10,13 +14,33 @@ import me.datafox.advent2022.SolutionBase;
  * @author datafox
  */
 public class Solution extends SolutionBase {
+    private static final String score = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static void main(String[] args) {
         new Solution().run();
     }
 
     @Override
     protected String solution1(String input) {
-        return "";
+        return String.valueOf(input.lines()
+                .map(this::getCommonChar)
+                .mapToInt(score::indexOf)
+                .sum());
+    }
+
+    private char getCommonChar(String s) {
+        Set<Character> set1 = new LinkedHashSet<>();
+        Set<Character> set2 = new LinkedHashSet<>();
+        int half = s.length() / 2;
+        for(int i = 0; i < s.length(); i++) {
+            if(i < half) {
+                set1.add(s.charAt(i));
+            } else {
+                set2.add(s.charAt(i));
+            }
+        }
+        set1.retainAll(set2);
+        return set1.iterator().next();
     }
 
     @Override
