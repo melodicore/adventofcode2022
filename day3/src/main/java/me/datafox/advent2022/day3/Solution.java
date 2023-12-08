b7ec55c6f56ce3ae181bb0fd40e798c3e6caa134
melodicore <datafox@datafox.me>
2023-12-08 21:51:27 +0200

    day 3 part 2

--- day3/src/main/java/me/datafox/advent2022/day3/Solution.java
+++ day3/src/main/java/me/datafox/advent2022/day3/Solution.java
@@ -2,8 +2,8 @@ package me.datafox.advent2022.day3;
 
 import me.datafox.advent2022.SolutionBase;
 
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
@@ -22,15 +22,36 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return String.valueOf(input.lines()
-                .map(this::getCommonChar)
+        return String.valueOf(input
+                .lines()
+                .map(this::getCommonChar1)
                 .mapToInt(score::indexOf)
                 .sum());
     }
 
-    private char getCommonChar(String s) {
-        Set<Character> set1 = new LinkedHashSet<>();
-        Set<Character> set2 = new LinkedHashSet<>();
+    @Override
+    protected String solution2(String input) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for(String s : input.split("\n")) {
+            sb.append(s).append("\n");
+            if(++i == 3) {
+                sb.append("\n");
+                i = 0;
+            }
+        }
+        return String.valueOf(Arrays
+                .stream(sb
+                        .toString()
+                        .split("\n\n"))
+                .map(this::getCommonChar2)
+                .mapToInt(score::indexOf)
+                .sum());
+    }
+
+    private char getCommonChar1(String s) {
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
         int half = s.length() / 2;
         for(int i = 0; i < s.length(); i++) {
             if(i < half) {
@@ -43,8 +64,23 @@ public class Solution extends SolutionBase {
         return set1.iterator().next();
     }
 
-    @Override
-    protected String solution2(String input) {
-        return "";
+    private char getCommonChar2(String s) {
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
+        Set<Character> set3 = new HashSet<>();
+        int set = 0;
+        for(String s1 : s.split("\n")) {
+            for(char c : s1.toCharArray()) {
+                switch(set) {
+                    case 0 -> set1.add(c);
+                    case 1 -> set2.add(c);
+                    case 2 -> set3.add(c);
+                }
+            }
+            set++;
+        }
+        set1.retainAll(set2);
+        set1.retainAll(set3);
+        return set1.iterator().next();
     }
 }
