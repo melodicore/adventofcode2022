9f675d11e6c67703149c2201078f7d98706e06f8
melodicore <datafox@datafox.me>
2023-12-08 21:16:32 +0200

    day 2 part 1

--- day2/src/main/java/me/datafox/advent2022/day2/Solution.java
+++ day2/src/main/java/me/datafox/advent2022/day2/Solution.java
@@ -10,17 +10,31 @@ import me.datafox.advent2022.SolutionBase;
  * @author datafox
  */
 public class Solution extends SolutionBase {
+    private static final String player1 = "ABC";
+    private static final String player2 = "XYZ";
     public static void main(String[] args) {
         new Solution().run();
     }
 
     @Override
     protected String solution1(String input) {
-        return "";
+        return String.valueOf(input.lines().mapToInt(this::getResult).sum());
     }
 
     @Override
     protected String solution2(String input) {
         return "";
+    }
+
+    private int getResult(String s) {
+        int i = player1.indexOf(s.charAt(0));
+        int j = player2.indexOf(s.charAt(2));
+        if(i == j) {
+            return j + 4;
+        }
+        if((i + 1) % 3 == j) {
+            return j + 7;
+        }
+        return j + 1;
     }
 }
