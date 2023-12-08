82b59daea4e8d7fa3cd328dbed45700111fad23c
melodicore <datafox@datafox.me>
2023-12-08 20:58:55 +0200

    doing 2022 just for fun

---
+++ template/src/main/java/me/datafox/advent2022/day/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent2022.day;
+
+import me.datafox.advent2022.SolutionBase;
+
+/**
+ * Solution for advent of code 2022 day ?.
+ *
+ * @see <a href=https://adventofcode.com/2022/day/?>https://adventofcode.com/2022/day/?</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
