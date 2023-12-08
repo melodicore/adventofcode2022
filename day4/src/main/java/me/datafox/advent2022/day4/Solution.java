2b7accff7f7f851a9e9b62ee9130020d0be68cab
melodicore <datafox@datafox.me>
2023-12-08 21:57:39 +0200

    start day 4

---
+++ day4/src/main/java/me/datafox/advent2022/day4/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent2022.day4;
+
+import me.datafox.advent2022.SolutionBase;
+
+/**
+ * Solution for advent of code 2022 day 4.
+ *
+ * @see <a href=https://adventofcode.com/2022/day/4>https://adventofcode.com/2022/day/4</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
