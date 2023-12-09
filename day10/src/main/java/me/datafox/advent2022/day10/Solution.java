95cc91a473d9142d61b9aaaa7d09a2ebebfadb10
melodicore <datafox@datafox.me>
2023-12-09 12:52:03 +0200

    day 10 part 1

--- day10/src/main/java/me/datafox/advent2022/day10/Solution.java
+++ day10/src/main/java/me/datafox/advent2022/day10/Solution.java
@@ -2,6 +2,8 @@ package me.datafox.advent2022.day10;
 
 import me.datafox.advent2022.SolutionBase;
 
+import java.util.Iterator;
+
 /**
  * Solution for advent of code 2022 day 10.
  *
@@ -16,7 +18,29 @@ public class Solution extends SolutionBase {
 
     @Override
     protected String solution1(String input) {
-        return "";
+        int x = 1;
+        int cycles = 0;
+        int strength = 0;
+        Iterator<String> it = input.lines().iterator();
+        int add = 0;
+        boolean mid = false;
+        while(it.hasNext()) {
+            cycles++;
+            if(cycles % 40 == 20) {
+                strength += x * cycles;
+            }
+            if(!mid) {
+                String op = it.next();
+                if(op.startsWith("addx")) {
+                    mid = true;
+                    add = Integer.parseInt(op.split(" ")[1]);
+                }
+            } else {
+                mid = false;
+                x += add;
+            }
+        }
+        return String.valueOf(strength);
     }
 
     @Override
