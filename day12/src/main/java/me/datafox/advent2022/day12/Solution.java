ce9c52639326dac2b8dbedb4f86adef641de8e86
melodicore <datafox@datafox.me>
2023-12-09 15:00:18 +0200

    start day 12

---
+++ day12/src/main/java/me/datafox/advent2022/day12/Solution.java
@@ -0,0 +1,26 @@
+package me.datafox.advent2022.day12;
+
+import me.datafox.advent2022.SolutionBase;
+
+/**
+ * Solution for advent of code 2022 day 12.
+ *
+ * @see <a href=https://adventofcode.com/2022/day/12>https://adventofcode.com/2022/day/12</a>
+ *
+ * @author datafox
+ */
+public class Solution extends SolutionBase {
+    public static void main(String[] args) {
+        new Solution().run();
+    }
+
+    @Override
+    protected String solution1(String input) {
+        return "";
+    }
+
+    @Override
+    protected String solution2(String input) {
+        return "";
+    }
+}
