136175c46d310dd85776a2a6ba2d8aac6dfed6ad
melodicore <datafox@datafox.me>
2023-12-09 14:17:01 +0200

    refactor BigInteger to long (arbitrary length not needed after all)

--- day11/src/main/java/me/datafox/advent2022/day11/Solution.java
+++ day11/src/main/java/me/datafox/advent2022/day11/Solution.java
@@ -2,7 +2,6 @@ package me.datafox.advent2022.day11;
 
 import me.datafox.advent2022.SolutionBase;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.function.Function;
@@ -29,13 +28,13 @@ public class Solution extends SolutionBase {
         int[] inspections = new int[monkeys.length];
         for(int i = 0; i < 20; i++) {
             for(int j = 0; j < monkeys.length; j++) {
-                LinkedList<BigInteger> items = monkeys[j].items;
+                LinkedList<Long> items = monkeys[j].items;
                 while(!items.isEmpty()) {
                     inspections[j]++;
-                    BigInteger item = items.removeFirst();
+                    long item = items.removeFirst();
                     item = monkeys[j].operation.apply(item);
-                    item = item.divide(BigInteger.valueOf(3));
-                    if(item.mod(monkeys[j].test).compareTo(BigInteger.ZERO) == 0) {
+                    item /= 3;
+                    if(item % monkeys[j].test == 0) {
                         monkeys[monkeys[j].ifTrue].items.add(item);
                     } else {
                         monkeys[monkeys[j].ifFalse].items.add(item);
@@ -54,19 +53,18 @@ public class Solution extends SolutionBase {
                 .map(this::getMonkey)
                 .toArray(Monkey[]::new);
         long[] inspections = new long[monkeys.length];
-        BigInteger max = Arrays
+        long max = Arrays
                 .stream(monkeys)
-                .map(Monkey::test)
-                .reduce(BigInteger::multiply)
-                .orElse(BigInteger.ONE);
+                .mapToLong(Monkey::test)
+                .reduce(1, (i, j) -> i * j);
         for(int i = 0; i < 10000; i++) {
             for(int j = 0; j < monkeys.length; j++) {
-                LinkedList<BigInteger> items = monkeys[j].items;
+                LinkedList<Long> items = monkeys[j].items;
                 while(!items.isEmpty()) {
                     inspections[j]++;
-                    BigInteger item = items.removeFirst().mod(max);
+                    long item = items.removeFirst() % max;
                     item = monkeys[j].operation.apply(item);
-                    if(item.mod(monkeys[j].test).compareTo(BigInteger.ZERO) == 0) {
+                    if(item % monkeys[j].test == 0) {
                         monkeys[monkeys[j].ifTrue].items.add(item);
                     } else {
                         monkeys[monkeys[j].ifFalse].items.add(item);
@@ -82,36 +80,36 @@ public class Solution extends SolutionBase {
         String[] split = s.split("\n");
         return new Monkey(Arrays
                 .stream(split[1].substring(18).split(", "))
-                .map(BigInteger::new)
+                .map(Long::parseLong)
                 .collect(Collectors.toCollection(LinkedList::new)),
                 getOperation(split[2].substring(19)),
-                new BigInteger(split[3].substring(21)),
+                Integer.parseInt(split[3].substring(21)),
                 Integer.parseInt(split[4].substring(29)),
                 Integer.parseInt(split[5].substring(30)));
     }
 
-    private Function<BigInteger,BigInteger> getOperation(String s) {
+    private Function<Long,Long> getOperation(String s) {
         String[] split = s.split(" ");
         return old -> {
-            BigInteger a, b;
+            long a, b;
             if(split[0].equals("old")) {
                 a = old;
             } else {
-                a = new BigInteger(split[0]);
+                a = Integer.parseInt(split[0]);
             }
             if(split[2].equals("old")) {
                 b = old;
             } else {
-                b = new BigInteger(split[2]);
+                b = Integer.parseInt(split[2]);
             }
             if(split[1].equals("+")) {
-                return a.add(b);
+                return a + b;
             } else {
-                return a.multiply(b);
+                return a * b;
             }
         };
     }
 
-    private record Monkey(LinkedList<BigInteger> items, Function<BigInteger,BigInteger> operation, BigInteger test, int ifTrue, int ifFalse) {
+    private record Monkey(LinkedList<Long> items, Function<Long,Long> operation, long test, int ifTrue, int ifFalse) {
     }
 }
